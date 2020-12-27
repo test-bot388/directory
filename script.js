e048c5755ed5d0bed93cd231fdae4b00a94e4535
Andrew Clark <andrewclark3942@gmail.com>
2020-12-27 18:27:28 -0500

    trying javascript

--- script.js
+++ script.js
@@ -1,117 +1,14 @@
+var jessieRogersContent = {
+    videoURL: [
+        "https://www.xvideos.com/video24297135/fucking_amazing_jessie_rogers", 
+    "https://www.xvideos.com/video20265337/loirinha_gostosa_tomando_pau_no_cuzinho",
+    "https://www.xvideos.com/video30143429/jessie_rogers",
+    "https://www.xvideos.com/video8924467/giant_cock_drills_the_juicy_pussy",],
+    videoPhoto: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTl4evwkQuyZku1M3reQXFUtYsanKrDy_wRSg&usqp=CAU",
+        "https://img-l3.xvideos-cdn.com/videos/thumbs169lll/91/42/de/9142de9d4d63138045aaebd9602c4774/9142de9d4d63138045aaebd9602c4774.11.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwpNpWVTNGhA0FKDwsTC8tX_tPXiNl2ZxEuA&usqp=CAU",
+        "https://img-hw.xnxx-cdn.com/videos/thumbslll/77/c5/c8/77c5c8d3f105fd2576ded1bd7df39ea6/77c5c8d3f105fd2576ded1bd7df39ea6.26.jpg",
+    ]
+};
 
-
-
-
-/**********
-var mark, john, markHeight, johnHeight, markMass, johnMass;
-markHeight = 1.7;
-johnHeight = 1.8;
-markMass = 100;
-johnMass = 110;
-var markBMI = markHeight / (markMass * markMass);
-var johnBMI = johnHeight / (johnMass * johnMass);
-
-console.log("Mark's BMI is" + " " + markBMI)
-console.log("John's BMI is" + " " + johnBMI)
-var markGreaterBMI = markBMI - johnBMI >= 0;
-console.log("Does Mark have a greater BMI than John?" + markGreaterBMI)
-
-
-********/
-
-
-/******
-
-var john;
-var johngame1 = 89;
-var johngame2 = 120;
-var johngame3 = 103;
-
-var mike;
-var mikegame1 = 116;
-var mikegame2 = 94;
-var mikegame3 = 123;
-
-var mary;
-var marygame1 = 97;
-var marygame2 = 134;
-var marygame3 = 135;
-
-var johnaverage = (johngame1 + johngame2 + johngame3) / 3;
-var mikeaverage = (mikegame1 + mikegame2 + mikegame3) / 3;
-var maryaverage = (marygame1 + marygame2 + marygame3) / 3;
-
-console.log(johnaverage)
-console.log(mikeaverage)
-console.log(maryaverage)
-
-if (johnaverage > mikeaverage && maryaverage) {
-    console.log('John had the highest average with ' + johnaverage)
-}
-else if (mikeaverage > johnaverage && maryaverage) {
-    console.log('Mike had the highest average with ' + mikeaverage)
-}
-else if (maryaverage > johnaverage && mikeaverage) {
-    console.log('Mary had the highest average with ' + maryaverage)
-}
-else console.log('John, Mike, and Mary tied in average scores')
-
-******/
-/**********************
-var john = {
-    height:2.6,
-    fullName:" John Smith",
-    weight: "160",
-    isMarried: "false",
-    bills: [128, 48, 268, 180, 42],
-    calcTips: function(){
-        this.tips = [];
-        this.finalValues = [];
-
-        for (var i = 0; i < this.bills.length; i++)
-        {
-            // determine percentage based on tipping rules
-            var percentage;
-            var bill = this.bills[i];
-
-            if (bill < 50) {
-                percentage = 0.2
-            } else if (bill >= 50 && < 200) {
-                percentage = 0.15
-            } else {
-                percentage = 0.1
-            }
-
-            //add results to corresponding arrays
-            this.tips[i] = bill * percentage
-        }
-    }
-}
-*********************** */
-
-
-let jim = {
-    bills: [128, 48, 268, 180, 42],
-    firstName: jim,
-    calcTips: function() {
-
-        //counter
-        for (var i = 0; i <= this.bills.length; i++)
-
-        let tips = [];
-        let finalCharge = [];
-        let percentage ;
-        let bill = this.bills[i]
-
-        if (bill < 50) {
-            percentage = 0.2
-        } else if (bill >= 50 && < 200) {
-            percentage = 0.15
-        } else {percentage = 0.1
-        }
-
-        tips = bill * percentage;
-        
-
-    }
-}
